ee289c13064dd29cc0b01019e7e015800d38ee00
tarek bahie <you@example.com>
2018-12-07 20:26:29 +0200

    revealVC implemented

---
+++ smack/Controller/ChatVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by tarek bahie on 12/7/18.
+//  Copyright © 2018 tarek bahie. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+
+    }
+
+}
