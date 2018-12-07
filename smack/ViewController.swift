166e7b7e9c660a28f2f42e35fc52f3c399f2cb46
tarek bahie <you@example.com>
2018-12-07 15:36:58 +0200

    Initial Commit

---
+++ smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack
+//
+//  Created by tarek bahie on 12/7/18.
+//  Copyright © 2018 tarek bahie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
