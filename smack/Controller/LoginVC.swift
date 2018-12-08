389fb2780c8dcdf575ee35db08433328df42b443
tarek bahie <you@example.com>
2018-12-08 12:54:05 +0200

    login UI updated

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by tarek bahie on 12/8/18.
+//  Copyright © 2018 tarek bahie. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by tarek bahie on 12/8/18.
+//  Copyright © 2018 tarek bahie. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
+
